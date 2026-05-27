4fcf9bab13a6af372e3eda5f86d052f003e98309
Levi_333 <leva.nechaev2008@gmail.com>
2026-05-27 18:31:46 +0300

    Creating repository

---
+++ app/src/main/java/com/example/FicusResourcesCounter/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.FicusResourcesCounter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // ХП Фикуса
+        val countFicusHP: TextView? = findViewById(R.id.countFicusHP);
+        val minusButtonFicusHP: Button = findViewById(R.id.minusButtonFicusHP)
+        val plusButtonFicusHP: Button = findViewById(R.id.plusButtonFicusHP)
+
+        if (countFicusHP?.text.toString() == "")
+            countFicusHP?.setText(MAX_FICUS_HP_COUNT.toString())
+
+        minusButtonFicusHP.setOnClickListener {
+            countFicusHP?.setText((countFicusHP.text!!.toString().toInt() - 1).toString())
+            if (countFicusHP?.text.toString().toInt() < 0)
+                countFicusHP?.setText(0.toString())
+        }
+        plusButtonFicusHP.setOnClickListener {
+            countFicusHP?.setText((countFicusHP.text!!.toString().toInt() + 1).toString())
+            if (countFicusHP?.text.toString().toInt() > MAX_FICUS_HP_COUNT)
+                countFicusHP?.setText(MAX_FICUS_HP_COUNT.toString())
+        }
+
+        // ХП Совомеда
+        val countOwlbearHP: TextView? = findViewById(R.id.countOwlbearHP);
+        val minusButtonOwlbearHP: Button = findViewById(R.id.minusButtonOwlbearHP)
+        val plusButtonOwlbearHP: Button = findViewById(R.id.plusButtonOwlbearHP)
+
+        if (countOwlbearHP?.text.toString() == "")
+            countOwlbearHP?.setText(MAX_OWLBEAR_HP_COUNT.toString())
+
+        minusButtonOwlbearHP.setOnClickListener {
+            countOwlbearHP?.setText((countOwlbearHP.text!!.toString().toInt() - 1).toString())
+            if (countOwlbearHP?.text.toString().toInt() < 0)
+                countOwlbearHP?.setText(0.toString())
+        }
+        plusButtonOwlbearHP.setOnClickListener {
+            countOwlbearHP?.setText((countOwlbearHP.text!!.toString().toInt() + 1).toString())
+            if (countOwlbearHP?.text.toString().toInt() > MAX_OWLBEAR_HP_COUNT)
+                countOwlbearHP?.setText(MAX_OWLBEAR_HP_COUNT.toString())
+        }
+
+        // Ячейки первого уровня
+        val countSpellI: TextView? = findViewById(R.id.countSpellI);
+        val minusButtonSpellI: Button = findViewById(R.id.minusButtonSpellI)
+        val plusButtonSpellI: Button = findViewById(R.id.plusButtonSpellI)
+
+        if (countSpellI?.text.toString() == "")
+            countSpellI?.setText(MAX_SPELL_I_COUNT.toString())
+
+        minusButtonSpellI.setOnClickListener {
+            countSpellI?.setText((countSpellI.text!!.toString().toInt() - 1).toString())
+            if (countSpellI?.text.toString().toInt() < 0)
+                countSpellI?.setText(0.toString())
+        }
+        plusButtonSpellI.setOnClickListener {
+            countSpellI?.setText((countSpellI.text!!.toString().toInt() + 1).toString())
+            if (countSpellI?.text.toString().toInt() > MAX_SPELL_I_COUNT)
+                countSpellI?.setText(MAX_SPELL_I_COUNT.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/example/FicusResourcesCounter/consts.kt
@@ -0,0 +1,7 @@
+package com.example.FicusResourcesCounter
+
+const val MAX_FICUS_HP_COUNT: Int = 63
+const val MAX_OWLBEAR_HP_COUNT: Int = 70
+const val MAX_SPELL_I_COUNT: Int = 3
+const val MAX_SPELL_II_COUNT: Int = 3
+const val MAX_SPELL_III_COUNT: Int = 2
