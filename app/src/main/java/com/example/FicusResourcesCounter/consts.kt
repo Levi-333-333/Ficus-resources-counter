76064e7498afccfaf1273d38a367fc24173a8fc2
Levi_333 <leva.nechaev2008@gmail.com>
2026-06-24 17:42:25 +0300

    У Фикуса праздник - лвлап! Изменены константы: максимальные хиты фикуса - 63 -> 74. Количество ячеек третьего уровня - 2 -> 3. Так же у совомеда появилась кличка Бари

--- app/src/main/java/com/example/FicusResourcesCounter/consts.kt
+++ app/src/main/java/com/example/FicusResourcesCounter/consts.kt
@@ -1,9 +1,9 @@
 package com.example.FicusResourcesCounter
 
-const val MAX_FICUS_HP_COUNT: Int = 63
+const val MAX_FICUS_HP_COUNT: Int = 74
 const val MAX_OWLBEAR_HP_COUNT: Int = 70
 const val MAX_SPELL_I_COUNT: Int = 3
 const val MAX_SPELL_II_COUNT: Int = 3
-const val MAX_SPELL_III_COUNT: Int = 2
+const val MAX_SPELL_III_COUNT: Int = 3
 const val MAX_FOCAL_SPELL_COUNT: Int = 2
 const val MAX_FICUS_FRUITS: Int = 10
