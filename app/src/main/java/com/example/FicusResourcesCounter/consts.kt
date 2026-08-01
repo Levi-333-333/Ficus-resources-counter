310465d5c8d413932b605034aab2515f88f16adf
Levi_333 <leva.nechaev2008@gmail.com>
2026-08-01 17:36:48 +0300

    Для Хитов Фикуса добавлены кнопки +5, +10, -5 и -10 (пока без функционала), изменён стиль кнопок

--- app/src/main/java/com/example/FicusResourcesCounter/consts.kt
+++ app/src/main/java/com/example/FicusResourcesCounter/consts.kt
@@ -1,10 +1,10 @@
 package com.example.FicusResourcesCounter
 
-const val MAX_FICUS_HP_COUNT: Int = 85
+const val MAX_FICUS_HP_COUNT: Int = 96
 const val MAX_OWLBEAR_HP_COUNT: Int = 70
 const val MAX_SPELL_I_COUNT: Int = 3
 const val MAX_SPELL_II_COUNT: Int = 3
 const val MAX_SPELL_III_COUNT: Int = 3
-const val MAX_SPELL_IV_COUNT: Int = 2
+const val MAX_SPELL_IV_COUNT: Int = 3
 const val MAX_FOCAL_SPELL_COUNT: Int = 2
 const val MAX_FICUS_FRUITS: Int = 10
